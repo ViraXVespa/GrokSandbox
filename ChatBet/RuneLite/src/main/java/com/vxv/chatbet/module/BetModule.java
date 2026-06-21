fb4526c62cdef6016996b4862e7192817280efee
Vira <viraxvespa@gmail.com>
2026-06-20 22:22:27 -0500

    Push core module and event files for the refactored ChatBet

---
+++ ChatBet/RuneLite/src/main/java/com/vxv/chatbet/module/BetModule.java
@@ -0,0 +1,16 @@
+package com.vxv.chatbet.module;
+
+import com.vxv.chatbet.bet.DropOutcome;
+import net.runelite.client.ui.overlay.components.PanelComponent;
+
+import java.util.Collections;
+import java.util.List;
+
+public interface BetModule {
+    String getName();
+    default void onActivate() {}
+    default void onDeactivate() {}
+    default void onItemContainerChanged(net.runelite.api.events.ItemContainerChanged event) {}
+    default List<DropOutcome> getSuggestedOutcomes() { return Collections.emptyList(); }
+    default void contributeToOverlay(PanelComponent panel) {}
+}
