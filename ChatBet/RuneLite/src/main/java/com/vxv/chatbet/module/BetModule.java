d1215701b1683792d0be289bf87202eaff4a9236
Vira <viraxvespa@gmail.com>
2026-06-22 17:45:23 -0500

    Fix module package and interface for compilation: added missing methods with defaults, standardized package to 'module'

--- ChatBet/RuneLite/src/main/java/com/vxv/chatbet/module/BetModule.java
+++ ChatBet/RuneLite/src/main/java/com/vxv/chatbet/module/BetModule.java
@@ -2,13 +2,25 @@ package com.vxv.chatbet.module;
 
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.StatChanged;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.client.ui.overlay.components.PanelComponent;
+import com.vxv.chatbet.bet.DropOutcome;
+
+import java.util.List;
 
 public interface BetModule {
     String getName();
     void onGameTick(GameTick event);
     void onStatChanged(StatChanged event);
+    default void onItemContainerChanged(ItemContainerChanged event) {}
+    default List<DropOutcome> getSuggestedOutcomes() {
+        return java.util.Collections.emptyList();
+    }
+    default void contributeToOverlay(PanelComponent panel) {}
     default long getElvesToGoal() {
         return 0;
     }
-    // Add other methods as modules expand
+    // Add other methods as modules expand. Include onDeactivate/onActivate if used.
+    default void onActivate() {}
+    default void onDeactivate() {}
 }
