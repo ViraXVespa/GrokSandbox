df769809806c2b20bc573529bd50d971b3f1da0b
Vira <viraxvespa@gmail.com>
2026-06-22 15:04:49 -0500

    Fix BetModule.java - full correct interface

--- ChatBet/RuneLite/src/main/java/com/vxv/chatbet/module/BetModule.java
+++ ChatBet/RuneLite/src/main/java/com/vxv/chatbet/module/BetModule.java
@@ -1,1 +1,21 @@
-default void onStatChanged(net.runelite.api.events.StatChanged event) {}
+package com.vxv.chatbet.module;
+
+import com.vxv.chatbet.bet.DropOutcome;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.events.StatChanged;
+import net.runelite.client.ui.overlay.components.PanelComponent;
+
+import java.util.Collections;
+import java.util.List;
+
+public interface BetModule {
+    String getName();
+    default void onActivate() {}
+    default void onDeactivate() {}
+    default void onItemContainerChanged(ItemContainerChanged event) {}
+    default void onGameTick(GameTick event) {}
+    default void onStatChanged(StatChanged event) {}
+    default List<DropOutcome> getSuggestedOutcomes() { return Collections.emptyList(); }
+    default void contributeToOverlay(PanelComponent panel) {}
+}
