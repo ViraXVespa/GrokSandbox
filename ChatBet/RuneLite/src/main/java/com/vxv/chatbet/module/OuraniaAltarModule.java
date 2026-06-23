ebae0d7ec783b0bbb3631e9fdf9f7c56cd137087
Vira <viraxvespa@gmail.com>
2026-06-23 02:52:14 -0500

    Add basic essence/pouch tracking fields and onItemContainerChanged skeleton

--- ChatBet/RuneLite/src/main/java/com/vxv/chatbet/module/OuraniaAltarModule.java
+++ ChatBet/RuneLite/src/main/java/com/vxv/chatbet/module/OuraniaAltarModule.java
@@ -1,14 +1,25 @@
 package com.vxv.chatbet.module;
 
 import com.vxv.chatbet.ChatBetPlugin;
+import net.runelite.api.InventoryID;
+import net.runelite.api.ItemContainer;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.ItemContainerChanged;
 import net.runelite.api.events.StatChanged;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class OuraniaAltarModule implements BetModule {
 
     private final ChatBetPlugin plugin;
+
+    // Basic tracking for essence and pouches
+    private final Map<Integer, Integer> lastInventoryQtys = new HashMap<>();
+    private final Map<Integer, Integer> lastPouchQtys = new HashMap<>();
+    private final AtomicInteger totalEssenceCarried = new AtomicInteger(0);
 
     public OuraniaAltarModule(ChatBetPlugin plugin) {
         this.plugin = plugin;
@@ -31,7 +42,28 @@ public class OuraniaAltarModule implements BetModule {
 
     @Override
     public void onItemContainerChanged(ItemContainerChanged event) {
-        // TODO: Track essence pouch + inventory essence changes
+        ItemContainer container = event.getItemContainer();
+        if (container == null) return;
+
+        if (container == plugin.getClient().getItemContainer(InventoryID.INVENTORY)) {
+            updateInventoryTracking(container);
+        } else if (isPouchContainer(container)) {
+            updatePouchTracking(container);
+        }
+    }
+
+    private void updateInventoryTracking(ItemContainer container) {
+        // TODO: Calculate delta for pure/daryalt essence
+        // Will be used to detect when player fills pouches or crafts
+    }
+
+    private void updatePouchTracking(ItemContainer container) {
+        // TODO: Track essence inside pouches (small, medium, large, giant)
+    }
+
+    private boolean isPouchContainer(ItemContainer container) {
+        // Placeholder - will check for pouch item IDs later
+        return false;
     }
 
     @Override
