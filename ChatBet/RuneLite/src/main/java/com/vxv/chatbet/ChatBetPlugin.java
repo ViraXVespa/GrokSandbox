5c7d107f8005d86e928d4a061a257f77f5f1230d
Vira <viraxvespa@gmail.com>
2026-06-24 09:32:34 -0500

    Remove old console debug logging block (moving to dedicated debug panel)

--- ChatBet/RuneLite/src/main/java/com/vxv/chatbet/ChatBetPlugin.java
+++ ChatBet/RuneLite/src/main/java/com/vxv/chatbet/ChatBetPlugin.java
@@ -176,10 +176,6 @@ public class ChatBetPlugin extends Plugin {
             if (currentXp > 0) lastThievingXp = currentXp;
         }
 
-        if (config.showDebugVars()) {
-            log.info("[ChatBet Debug] ...");
-        }
-
         // Simple periodic poll of bridge for stream chat interop (expand in future commits)
         if (System.currentTimeMillis() % 5000 < 100) { // rough ~5s interval
             pollBridgeForNonCommandChat();
